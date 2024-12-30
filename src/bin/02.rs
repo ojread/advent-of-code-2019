2b85f52a28f99c70afc90a7827c17deed26bddd3
Oliver Read <oliver@reads.org.uk>
2024-12-30 10:37:33 +0000

    Solved day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,49 +1,36 @@
 advent_of_code::solution!(2);
 
+use advent_of_code::Intcode;
+
 pub fn part_one(input: &str) -> Option<usize> {
-    let mut state: Vec<usize> = input
-        .split(",")
-        .map(|int| int.trim().parse::<usize>().unwrap())
-        .collect();
+    let mut intcode = Intcode::new(input);
+    intcode.set_noun(12);
+    intcode.set_verb(2);
+    let output = intcode.run();
+    Some(output)
+}
 
-    let mut running: bool = true;
-    let mut index: usize = 0;
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut intcode = Intcode::new(input);
 
-    while running {
-        let opcode: usize = state[index];
+    let target_output = 19690720;
 
-        match opcode {
-            1 => {
-                let a: usize = state[state[index + 1]];
-                let b: usize = state[state[index + 2]];
-                let c: usize = state[index + 3];
-                state[c] = a + b;
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            intcode.set_noun(noun);
+            intcode.set_verb(verb);
+            let output = intcode.run();
+            // println!("{}", output);
+            if output == target_output {
+                // println!("{}, {}", noun, verb);
+                // Return the noun and verb as a single number.
+                return Some(noun * 100 + verb);
             }
-            2 => {
-                let a: usize = state[state[index + 1]];
-                let b: usize = state[state[index + 2]];
-                let c: usize = state[index + 3];
-                state[c] = a * b;
-            }
-            99 => {
-                running = false;
-            }
-            _ => {}
         }
-        index += 4;
     }
 
-    println!("{:?}", state);
-
-    Some(state[0])
-}
-
-pub fn part_two(input: &str) -> Option<u32> {
+    // If we reached here then the puzzle hasn't been solved.
     None
-}
-
-fn add(a: u32, b: u32) -> u32 {
-    a + b
 }
 
 #[cfg(test)]
@@ -59,6 +46,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(8444));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,82 @@
 pub mod template;
 
 // Use this file to add helper functions and additional modules.
+
+pub struct Intcode {
+    program: Vec<usize>,
+    memory: Vec<usize>,
+    pointer: usize,
+    noun: Option<usize>,
+    verb: Option<usize>,
+}
+
+impl Intcode {
+    pub fn new(state: &str) -> Intcode {
+        Intcode {
+            program: state
+                .split(",")
+                .map(|int| int.trim().parse::<usize>().unwrap())
+                .collect(),
+            memory: vec![0],
+            pointer: 0,
+            noun: None,
+            verb: None,
+        }
+    }
+
+    pub fn set_noun(&mut self, noun: usize) {
+        self.noun = Some(noun);
+    }
+
+    pub fn set_verb(&mut self, verb: usize) {
+        self.verb = Some(verb);
+    }
+
+    pub fn run(&mut self) -> usize {
+        // Copy program into memory.
+        self.memory.clone_from(&self.program);
+
+        // set noun and verb if set.
+        if self.noun.is_some() {
+            self.memory[1] = self.noun.unwrap();
+        }
+        if self.verb.is_some() {
+            self.memory[2] = self.verb.unwrap();
+        }
+
+        let mut running: bool = true;
+        self.pointer = 0;
+
+        while running {
+            let opcode: usize = self.memory[self.pointer];
+
+            match opcode {
+                1 => self.add(),
+                2 => self.multiply(),
+                99 => {
+                    running = false;
+                }
+                other => {
+                    println!("Unknown opcode {}", other)
+                }
+            }
+        }
+        self.memory[0]
+    }
+
+    fn add(&mut self) {
+        let a: usize = self.memory[self.memory[self.pointer + 1]];
+        let b: usize = self.memory[self.memory[self.pointer + 2]];
+        let c: usize = self.memory[self.pointer + 3];
+        self.memory[c] = a + b;
+        self.pointer += 4;
+    }
+
+    fn multiply(&mut self) {
+        let a: usize = self.memory[self.memory[self.pointer + 1]];
+        let b: usize = self.memory[self.memory[self.pointer + 2]];
+        let c: usize = self.memory[self.pointer + 3];
+        self.memory[c] = a * b;
+        self.pointer += 4;
+    }
+}
