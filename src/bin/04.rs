5e47bca1ba63e63b0c6319c45bd0d98c2c1193f9
Oliver Read <oliver@reads.org.uk>
2025-01-02 15:32:30 +0000

    Solved day 4 part 1

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,10 +1,51 @@
 advent_of_code::solution!(4);
 
-pub fn part_one(input: &str) -> Option<u64> {
-    None
+pub fn part_one(input: &str) -> Option<usize> {
+    let boundaries: Vec<u32> = input
+        .split("-")
+        .map(|boundary| {
+            boundary
+                .trim()
+                .parse()
+                .expect("Boundary couldn't be parsed.")
+        })
+        .collect();
+
+    // .map(|boundary| boundary.parse().expect("Bounday couldn't be parsed."))
+    let lower: u32 = boundaries[0];
+    let upper: u32 = boundaries[1];
+
+    let passwords = (lower..=upper)
+        .filter(|num| {
+            // Password criteria.
+            let mut has_double = false;
+            let mut has_decreasing = false;
+
+            let password = num.to_string();
+
+            let mut chars = password.chars().peekable();
+
+            while let Some(this) = chars.next() {
+                if let Some(next) = chars.peek() {
+                    if this == *next {
+                        has_double = true;
+                    }
+                    let d1 = this.to_digit(10).expect("Nah");
+                    let d2 = next.to_digit(10).expect("Nah");
+                    if d1 > d2 {
+                        has_decreasing = true;
+                    }
+                }
+            }
+
+            (has_double && !has_decreasing)
+        })
+        .count();
+
+    Some(passwords)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+pub fn part_two(_input: &str) -> Option<u64> {
     None
 }
 
