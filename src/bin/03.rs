418624d582fd30105ed93f5e8f20c0404aacc3f0
Oliver Read <oliver@reads.org.uk>
2025-01-03 20:49:42 +0000

    Update example data

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -190,12 +190,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(159));
+        assert_eq!(result, Some(4981));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(610));
+        assert_eq!(result, Some(164012));
     }
 }
